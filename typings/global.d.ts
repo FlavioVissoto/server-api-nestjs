fdb18cf455c2cb510eddbcea2b66faca146e9e4a
FlavioVissoto <vissoto_flavio@hotmail.com>
2023-04-11 17:26:23 -0300

    Feat: Global typings

---
+++ typings/global.d.ts
@@ -0,0 +1,20 @@
+export declare global {
+  type AnyObject = Record<string, unknown>;
+
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: string;
+      PORT: string;
+
+      DATABASE_HOST: string;
+      DATABASE_USER: string;
+      DATABASE_SCHEMA: string;
+      DATABASE_DATABASE: string;
+      DATABASE_PASSWORD: string;
+      DATABASE_PORT: string;
+
+      JWT_SECRET: string;
+      JWT_EXPIRES_TIME: string;
+    }
+  }
+}
